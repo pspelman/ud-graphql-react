39b100ceb9f8243a075ff686dfa63ccd9e0ab9fb
Phil Spelman <phil_spelman@hotmail.com>
2021-09-17 13:58:47 -0500

    - added json-server - added db.json with users and companies - hooked up first associated types (User and Company)

--- intro-users/schema/schema.js
+++ intro-users/schema/schema.js
@@ -1,18 +1,38 @@
 const graphql = require("graphql");
+const axios = require("axios");
+const { GraphQLID } = require("graphql");
 const { GraphQLString, GraphQLInt, GraphQLSchema, GraphQLObjectType } = graphql;
-const _ = require("lodash");
+// const _ = require("lodash");
 
 const users = [
-  { id: "23", firstName: "Bill", age: 20 },
-  { id: "47", firstName: "Samantha", age: 20 },
+  { id: 1, firstName: "Bill", age: 20 },
+  { id: 2, firstName: "Samantha", age: 20 },
 ];
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
 
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
-    id: { type: GraphQLString },
+    id: { type: GraphQLInt },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        // args.companyId
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((resp) => resp.data);
+      },
+    },
   },
 });
 
@@ -22,9 +42,12 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     user: {
       type: UserType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLInt } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        // return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((resp) => resp.data);
       },
     },
   },
